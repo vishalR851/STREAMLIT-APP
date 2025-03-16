8c8e9584f546d6f40b15c71a524cab7fb473faf9
vishalR851 <vishalramajeyam@gmail.com>
2025-03-16 15:01:57 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,7 +10,7 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
